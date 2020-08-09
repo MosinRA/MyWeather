534e61d4a8c249b57448d24955d3bd7cf26723ed
MosinRA <mosinra90@yandex.ru>
2020-08-09 21:13:00 +0500

    HW5

--- app/src/main/java/com/mosin/myweather/MainActivity.java
+++ app/src/main/java/com/mosin/myweather/MainActivity.java
@@ -1,85 +1,23 @@
 package com.mosin.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button toFindCityBtn, infoCityBtn, infoYandexBtn;
-    private TextView textViewCity;
-    private ImageView settingBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViews();
-        setBtnToFindCityOnClick();
-        showFromFindCityActivity();
-        showSettingBtnOnClick();
-        showInfoCityBtnOnClick();
-        showInfoYandexOnClick();
     }
-
-    private void findViews(){
-        toFindCityBtn = findViewById(R.id.findCityBtn);
-        textViewCity = findViewById(R.id.city);
-        settingBtn = findViewById(R.id.icoSetting);
-        infoCityBtn = findViewById(R.id.infoCity);
-        infoYandexBtn = findViewById(R.id.yandex);
-    }
-
-    private void setBtnToFindCityOnClick(){
-        toFindCityBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, FindCity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void showFromFindCityActivity(){
-        String city = getIntent().getStringExtra(FindCity.cityKey);
-        textViewCity.setText(city);
-    }
-
-    private void showSettingBtnOnClick(){
-        settingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Setting.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void showInfoCityBtnOnClick() {
-        infoCityBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("https://wikipedia.org/wiki/" + textViewCity.getText().toString());
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void showInfoYandexOnClick() {
-        infoYandexBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("https://yandex.ru/pogoda/" + textViewCity.getText().toString());
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        int countOfFragmentInManager = getSupportFragmentManager().getBackStackEntryCount();
+        if(countOfFragmentInManager > 0) {
+            getSupportFragmentManager().popBackStack();
+        }
     }
 }
 
